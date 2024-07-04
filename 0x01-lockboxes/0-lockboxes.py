1db8e1d10b88222d9841049174fbcc3ea52e9686
Elijah <eachala5@gmail.com>
2024-07-04 12:45:52 +0000

    1

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,17 +1,13 @@
 #!/usr/bin/python3
-"""Locked boxes module"""
-
+'''Locked boxes module'''
 
 def canUnlockAll(boxes):
-    """Determines if all the boxes can be opened or not
-    Returns:
-        True: if all boxes can be opened
-        False: otherwise
-    """
+    '''function'''
+
     max_idx = len(boxes) - 1
     keys = [0] + boxes[0]
     opened = [0]
-    
+
     while keys:
         if keys[0] in opened:
             keys.remove(keys[0])
@@ -21,7 +17,11 @@ def canUnlockAll(boxes):
             keys += boxes[keys[0]]
             opened.append(keys[0])
             keys.remove(keys[0])
-    
+
     if len(opened) == max_idx + 1:
         return True
-    return False
+    for i in range(max_idx + 1):
+        if i not in opened:
+            if boxes[i]:
+                return False
+    return True
