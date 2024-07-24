58966f3e6b09c266ad8e4e60bb1a9bbfca8eafca
Elijah <eachala5@gmail.com>
2024-07-24 15:49:21 +0000

    00

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -15,22 +15,22 @@ def signal_handler(sig, frame):
         print('{}: {}'.format(i, codes[i]))
     sys.exit(0)
 
+if __name__ == "__main__":
+    signal.signal(signal.SIGINT, signal_handler)
+    fsize = 0
+    codes = {'200': 0, '301': 0, '400': 0, '401': 0, '403': 0, '404': 0, '405':0, '500': 0}
+    log_pattern = re.compile(r'(\d+\.\d+\.\d+\.\d+) - \[(.+)\] "GET /projects/260 HTTP/1.1" (\d{3}) (\d+)')
 
-signal.signal(signal.SIGINT, signal_handler)
-fsize = 0
-codes = {'200': 0, '301': 0, '400': 0, '401': 0, '403': 0, '404': 0, '405':0, '500': 0}
-log_pattern = re.compile(r'(\d+\.\d+\.\d+\.\d+) - \[(.+)\] "GET /projects/260 HTTP/1.1" (\d{3}) (\d+)')
 
-
-while True:
-    for j in range(10):
-        line = sys.stdin.readline()
-        if log_pattern.match(line):
-            words = line.split(" ")
-            l = len(words)
-            fsize += int(words[l - 1])
-            if words[l - 2] in codes:
-                codes[words[l - 2]] += 1
-    print('file size: {:d}'.format(fsize))
-    for i in codes:
-        print('{}: {}'.format(i, codes[i]))
+    while True:
+        for j in range(10):
+            line = sys.stdin.readline()
+            if log_pattern.match(line):
+                words = line.split(" ")
+                l = len(words)
+                fsize += int(words[l - 1])
+                if words[l - 2] in codes:
+                    codes[words[l - 2]] += 1
+        print('file size: {:d}'.format(fsize))
+        for i in codes:
+            print('{}: {}'.format(i, codes[i]))
