c38ce6911ea250ce6f10df767a8d70a80ab83488
Your Name <you@example.com>
2024-07-24 16:54:53 +0000

    000001

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -19,9 +19,8 @@ def signal_handler(sig, frame):
 if __name__ == "__main__":
     signal.signal(signal.SIGINT, signal_handler)
     fsize = 0
-    codes = {'200': 0, '301': 0, '400': 0, '401': 0, '403': 0, '404': 0, '405':0, '500': 0}
     log_pattern = re.compile(r'(\d+\.\d+\.\d+\.\d+) - \[(.+)\] "GET /projects/260 HTTP/1.1" (\d{3}) (\d+)')
-
+    codes = {'200': 0, '301': 0, '400': 0, '401': 0, '403': 0, '404': 0, '405':0, '500': 0}
 
     while True:
         for j in range(10):
