8412e7c313013d591473a29c51b4e5b582066260
Your Name <you@example.com>
2024-07-24 20:35:11 +0000

    111

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -24,14 +24,24 @@ if __name__ == "__main__":
 
     while True:
         for j in range(10):
-            line = sys.stdin.readline()
-            if log_pattern.match(line):
-                words = line.split(" ")
-                l = len(words)
-                fsize += int(words[l - 1])
-                if words[l - 2] in codes:
-                    codes[words[l - 2]] += 1
+            try:
+                line = input()#sys.stdin.readline()
+                if log_pattern.match(line):
+                    words = line.split(" ")
+                    l = len(words)
+                    fsize += int(words[l - 1])
+                    if words[l - 2] in codes:
+                        codes[words[l - 2]] += 1
+            except(KeyboardInterrupt, EOFError):
+                print('file size: {:d}'.format(fsize))
+                for i in codes:
+                    if codes[i]:
+                        print('{}: {}'.format(i, codes[i]))
+                sys.exit(0)
+
+
         print('file size: {:d}'.format(fsize))
         for i in codes:
             if codes[i]:
                 print('{}: {}'.format(i, codes[i]))
+
